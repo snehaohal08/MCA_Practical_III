bf4cbdd32b45439c07966d88a9adaf99452f2502
snehaohal08 <snehaohal08@gmail.com>
2025-10-18 01:01:36 +0530

    q9

--- Mern/Nodejs/routes/studentRoutes.js
+++ Mern/Nodejs/routes/studentRoutes.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Student = require('../models/student');
+const Student = require("../models/student");
 
 // Create a student
-router.post('/', async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   try {
     const student = await Student.create(req.body);
     res.status(201).json(student);
@@ -11,9 +11,8 @@ router.post('/', async (req, res, next) => {
     next(err);
   }
 });
-
 // Read all students
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const students = await Student.find();
     res.json(students);
@@ -21,14 +20,30 @@ router.get('/', async (req, res, next) => {
     next(err);
   }
 });
-
-router.delete('/:id', async (req, res, next) => {
+// Delete  students
+router.delete("/:id", async (req, res, next) => {
   try {
     const students = await Student.findByIdAndDelete(req.params.id);
-    if (!students) return res.status(404).json({ message: 'Product not found' });
-    res.status(200).json({ message: 'Product deleted successfully' });
+    if (!students)
+      return res.status(404).json({ message: "student not found" });
+    res.status(200).json({ message: "student deleted successfully" });
   } catch (err) {
     next(err);
+  }
+});
+// Update student
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedStudent = await Student.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true }
+    );
+    if (!updatedStudent)
+      return res.status(404).json({ message: "Student not found" });
+    res.json({ data: updatedStudent, message: "Updated successfully" });
+  } catch (err) {
+    res.status(400).json({ message: "Bad Request", error: err.message });
   }
 });
 
